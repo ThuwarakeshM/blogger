bf607243a37af7de0975c5642d2b3dbc1c984025
Thuwarakesh <thuwarakesh@gmail.com>
2018-12-27 17:28:08 +0530

    home page intro text

--- home/models.py
+++ home/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname='full'),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context =  super().get_context(request, *args, **kwargs)
+        # Add additional context here
+        return context
