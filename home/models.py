77b309faff673a9149ef3b503f0eeb33ecf262f0
Thuwarakesh <thuwarakesh@gmail.com>
2019-01-03 07:28:34 +0530

    added dynamic sidebars to homepage

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from blog.models import BlogPage
 
 
 class HomePage(Page):
@@ -15,4 +16,9 @@ class HomePage(Page):
     def get_context(self, request, *args, **kwargs):
         context =  super().get_context(request, *args, **kwargs)
         # Add additional context here
+
+        context['hot'] = BlogPage.objects.filter(hot_news=True).live().last()
+        context['editors_picks'] = BlogPage.objects.filter(editors_pick=False).live().order_by('-first_published_at')[:3]
+        context['popular'] = BlogPage.objects.live().order_by('-views')[:5]
+
         return context
