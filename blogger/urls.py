2bc783a5138cac7d058ad55c292bdae60765f0f1
Thuwarakesh <thuwarakesh@gmail.com>
2019-01-07 12:25:19 +0530

    sitemap included

--- blogger/urls.py
+++ blogger/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -9,6 +10,9 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 
 urlpatterns = [
+
+    url(r'^sitemap\.xml$', sitemap),
+
     url(r'^django-admin/', admin.site.urls),
 
     url(r'^admin/', include(wagtailadmin_urls)),
