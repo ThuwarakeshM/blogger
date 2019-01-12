eb7a0e954dc8dfd1605a6ee424129f10688ef9d6
Thuwarakesh <thuwarakesh@gmail.com>
2019-01-12 11:01:01 +0530

    fix: search page appearing in navmenu

--- blog/templatetags/navbar.py
+++ blog/templatetags/navbar.py
@@ -1,6 +1,6 @@
 from django import template
 from home.models import HomePage
-from blog.models import BlogIndexPage
+from blog.models import BlogIndexPage, BlogSearchPage
 
 register = template.Library()
 
@@ -9,6 +9,6 @@ def navbar():
 
     home_page = HomePage.objects.get()
 
-    ix_pages = home_page.get_children().type(BlogIndexPage)
+    ix_pages = home_page.get_children().type(BlogIndexPage).not_type(BlogSearchPage)
 
     return {'ix_pages': ix_pages, 'home_page': home_page}
